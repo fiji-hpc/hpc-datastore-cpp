96e407d6a0b7dcfad3882315e8e9867d4c6e3b3a
xjurack1 <xjurack1@fi.muni.cz>
2022-05-31 15:45:47 +0200

    work on api

---
+++ src/hpc_ds_api.hpp
@@ -0,0 +1,144 @@
+#pragma once
+#include <array>
+#include <i3d/image3d.h>
+#include <string>
+#include <type_traits>
+
+namespace details {
+template <typename T>
+concept Scalar = requires(T) {
+	std::is_scalar_v<T>;
+};
+} // namespace details
+
+namespace datastore {
+template <typename T>
+class Vector3D {
+  public:
+	Vector3D() = default;
+	Vector3D(T xyz) : _values({xyz, xyz, xyz}) {}
+	Vector3D(T x, T y, T z) : _values({x, y, z}) {}
+
+	T& x() { return _values[0]; }
+	T& y() { return _values[1]; }
+	T& z() { return _values[2]; }
+
+	T x() const { return _values[0]; }
+	T y() const { return _values[1]; }
+	T z() const { return _values[2]; }
+
+	T& operator[](std::size_t idx) { return _values[idx]; }
+	T operator[](std::size_t idx) const { return _values[idx]; }
+
+  private:
+	std::array<T, 3> _values;
+};
+
+class ImageView {
+  public:
+	ImageView(std::string url,
+	          std::string uuid,
+	          int channel,
+	          int timepoint,
+	          int angle,
+	          Vector3D<int> resolution,
+	          std::string version);
+
+	template <details::Scalar T>
+	i3d::Image3d<T> read_block(Vector3D<int> coords) const;
+
+	template <details::Scalar T>
+	std::vector<i3d::Image3d<T>>
+	read_blocks(const std::vector<Vector3D<int>>& coords) const;
+
+	template <details::Scalar T>
+	i3d::Image3d<T> read_image() const;
+
+	template <details::Scalar T>
+	bool write_block(const i3d::Image3d<T>& block, Vector3D<int> coords) const;
+
+	template <details::Scalar T>
+	bool write_blocks(const std::vector<const i3d::Image3d<T>*>& blocks,
+	                  const std::vector<Vector3D<int>>& coords) const;
+
+	template <details::Scalar T>
+	bool write_image(const i3d::Image3d<T>& img) const;
+
+  private:
+	std::string _url;
+	std::string _uuid;
+	int _channel;
+	int _timepoint;
+	int _angle;
+	Vector3D<int> _resolution;
+	std::string _version;
+};
+
+/** TODO check types and finish function headings **/
+class Connection {
+  public:
+	Connection(std::string url, std::string uuid);
+
+	template <details::Scalar T>
+	i3d::Image3d<T> read_block(Vector3D<int> coords,
+	                           int channel,
+	                           int timepoint,
+	                           int angle,
+	                           Vector3D<int> resolution,
+	                           const std::string& version) const;
+
+	template <details::Scalar T>
+	std::vector<i3d::Image3d<T>> read_blocks(std::vector<Vector3D<int>> coords,
+	                                         int channel,
+	                                         int timepoint,
+	                                         int angle,
+	                                         Vector3D<int> resolution,
+	                                         const std::string& version) const;
+
+	template <details::Scalar T>
+	i3d::Image3d<T> read_image(int channel,
+	                           int timepoint,
+	                           int angle,
+	                           Vector3D<int> resolution,
+	                           const std::string& version) const;
+
+	template <details::Scalar T>
+	std::vector<i3d::Image3d<T>>
+	read_timepoints(int channel,
+	                std::vector<int> timepoints,
+	                int angle,
+	                Vector3D<int> resolution,
+	                const std::string& version) const;
+
+  private:
+	std::string _url;
+	std::string _uuid;
+};
+
+template <details::Scalar T>
+i3d::Image3d<T> read_image(const std::string& url,
+                           const std::string& uuid,
+                           int channel = 0,
+                           int timepoint = 0,
+                           int angle = 0,
+                           Vector3D<int> resolution = {1, 1, 1},
+                           const std::string& version = "latest");
+
+template <details::Scalar T>
+bool write_image(const i3d::Image3d<T>& img,
+                 const std::string& url,
+                 const std::string& uuid,
+                 int channel = 0,
+                 int timepoint = 0,
+                 int angle = 0,
+                 Vector3D<int> resolution = {1, 1, 1},
+                 const std::string version = "latest");
+
+} // namespace datastore
+
+/* ================= IMPLEMENTATION FOLLOWS ======================== */
+
+namespace datastore {
+Connection::Connection(std::string url, std::string uuid)
+    : _url(std::move(url)), _uuid(std::move(uuid)) {}
+} // namespace datastore
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <fmt/core.h>
 #include <iostream>
 
-int main() { std::cout << fmt::format("Hello world with code: {}\n", 69); }
+namespace x {
+int x = 4;
+int foo() { return 5; }
+} // namespace x
+
+namespace y {
+using namespace x;
+int bar() { return foo(); }
+} // namespace y
+
+int main() {
+	std::cout << fmt::format("Hello world with code: {}\n", y::foo());
+}
 

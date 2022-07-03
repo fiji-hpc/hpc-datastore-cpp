aca1c9a16da45f7c024a939d52cfb99a8478afae
xjurack1 <xjurack1@fi.muni.cz>
2022-07-03 21:18:08 +0200

    adding unit test

--- tests/units/read_block.hpp
+++ tests/units/read_block.hpp
@@ -44,9 +44,44 @@ void test_read_block() {
 	}
 	phase_ok();
 
-	phase_start("Offsets");
+	phase_start("Read offsets");
 
-    phase_ok();
+	auto block_size = props.get_block_size({0, 0, 0}, IMG_RESOLUTION);
+	i3d::Image3d<T> random_block;
+	random_block.MakeRoom(block_size);
+	fill_random(random_block);
+	view.write_block(random_block, {0, 0, 0}, {0, 0, 0});
+
+	i3d::Image3d<T> view_img;
+	view_img.MakeRoom(block_size * 2);
+
+	i3d::Image3d<T> conn_img;
+	conn_img.MakeRoom(block_size * 2);
+
+	for (int x = 0; x < block_size.x; x += block_size.x / 10)
+		for (int y = 0; y < block_size.y; y += block_size.y / 10)
+			for (int z = 0; z < block_size.z; z += block_size.z / 10) {
+				i3d::Vector3d<int> offset{x, y, z};
+				view.read_block({0, 0, 0}, view_img, offset);
+				conn.read_block({0, 0, 0}, conn_img, offset, IMG_CHANNEL,
+				                IMG_TIMEPOINT, IMG_ANGLE, IMG_RESOLUTION,
+				                IMG_VERSION);
+
+				for (int xx = 0; xx < block_size.x; ++xx)
+					for (int yy = 0; yy < block_size.y; ++yy)
+						for (int zz = 0; zz < block_size.z; ++zz) {
+							i3d::Vector3d<std::size_t> coord{std::size_t(xx),
+							                                 std::size_t(yy),
+							                                 std::size_t(zz)};
+							assert(random_block.GetVoxel(coord) ==
+							       view_img.GetVoxel(coord + offset));
+
+							assert(random_block.GetVoxel(coord) ==
+							       conn_img.GetVoxel(coord + offset));
+						}
+			}
+
+	phase_ok();
 
 	test_ok();
 }

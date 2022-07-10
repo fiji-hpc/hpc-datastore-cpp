d5eae3bb683694118d1583032828a0e12ad3d79c
xjurack1 <xjurack1@fi.muni.cz>
2022-07-10 16:01:49 +0200

    example pybind creation

--- python_binding/source_code/src/main.cpp
+++ python_binding/source_code/src/main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
-#include <cassert>
 
-int main()
-{
-    std::cout << "Hello world\n";
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+int add(int i, int j) { return i + j; }
+
+PYBIND11_MODULE(main, m) {
+	m.doc() = "pybind11 example plugin"; // optional module docstring
+
+	m.def("add", &add, "A function that adds two numbers");
 }
